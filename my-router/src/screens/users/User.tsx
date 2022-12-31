7e0c65f15984d469d16663c2298007a438474299
kyungchandevelop@gmail.com <championcom@naver.com>
2022-12-31 18:46:04 +0900

    Outlet

--- my-router/src/screens/users/User.tsx
+++ my-router/src/screens/users/User.tsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { Link, Outlet, useParams } from "react-router-dom";
 import { users } from "../../db";
 
 export default function User() {
@@ -8,6 +8,9 @@ export default function User() {
       <h1>
         {params.userId} {users[Number(params.userId) - 1].name}
       </h1>
+      <hr />
+      <Link to="followers">See Followers</Link>
+      <Outlet />
     </div>
   );
 }
